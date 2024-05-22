8e0ba99a4ee369e9c91d7642cc6931955f135c3c
EnzoSantoss <enzoaugusto312@gmail.com>
2024-05-22 19:43:47 -0300

    primeiro ex de logica em array 2d

--- src/binary_search/binary_search_2d.ts
+++ src/binary_search/binary_search_2d.ts
@@ -1,11 +1,11 @@
-const matrix = [
-  [3, 4, 5, 6],
-  [7, 8, 9, 11],
-  [14, 16, 17, 20],
-  [33, 54, 58, 99],
-];
+// const matrix = [
+//   [3, 4, 5, 6],
+//   [7, 8, 9, 11],
+//   [14, 16, 17, 20],
+//   [33, 54, 58, 99],
+// ];
 
-search(matrix, 9);
+//search(matrix, 9);
 
 function search(matrix: number[][], target: number) {
   let row = 0;
@@ -28,4 +28,60 @@ function search(matrix: number[][], target: number) {
 
   console.log("not found");
   return [-1, -1];
+}
+
+//Ex 1 Binary search em um array 2d
+
+const matrix = [
+  [3, 4, 5, 6],
+  [7, 8, 9, 11],
+  [14, 16, 17, 20],
+  [33, 54, 58, 99],
+];
+
+//searchRoll(matrix, 8);
+//Retorna a possivel linha onde esta o alvo
+const possibleRow = searchRoll(matrix, 9);
+function searchRoll(matrix: number[][], target: number) {
+  let start = 0;
+  let end = matrix.length - 1;
+
+  while (start <= end) {
+    const mid = Math.floor((start + end) / 2);
+
+    if (matrix[mid][0] == target) {
+      console.log(`VALOR ENCONTRADO EM ROW (${mid}) COLUNA (0)`);
+      return mid;
+    } else if (matrix[mid][0] < target) {
+      start = mid + 1;
+    } else if (matrix[mid][0] > target) {
+      end = mid - 1;
+    }
+  }
+
+  console.log(`POTENCIAL ROW: ${end}`);
+  return end;
+}
+
+searchOverRoll(matrix, possibleRow, 9);
+
+function searchOverRoll(matrix: number[][], row: number, target: number) {
+  const targetRow = matrix[row];
+  let start = 0;
+  let end = targetRow.length;
+
+  while (start <= end) {
+    const mid = Math.floor((start + end) / 2);
+
+    if (targetRow[mid] == target) {
+      console.log(`VALUE FOUND ON ROW (${row}) | COL (${mid})`);
+      return mid;
+    } else if (targetRow[mid] < target) {
+      start = mid + 1;
+    } else if (targetRow[mid] > target) {
+      end = mid - 1;
+    }
+  }
+
+  console.log(`VALUE NOT FOUND`);
 }
